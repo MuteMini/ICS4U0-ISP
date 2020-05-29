63e03d9b15033320e866a469befaeaf036faebad
MuteMini <63823931+MuteMini@users.noreply.github.com>
2020-05-29 00:39:12 -0400

    Creates new files for rough bus map

---
+++ plans/level-design/bus-map/busSprite.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="busSprite" tilewidth="32" tileheight="32" tilecount="64" columns="8">
+ <image source="../busSprite.png" width="256" height="256"/>
+</tileset>
