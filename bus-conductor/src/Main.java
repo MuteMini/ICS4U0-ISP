62dea0d1432055108aa2a3de58c656036938bfe5
MuteMini <63823931+MuteMini@users.noreply.github.com>
2020-05-31 02:47:21 -0400

    fix screen height problem

--- bus-conductor/src/Main.java
+++ bus-conductor/src/Main.java
@@ -3,13 +3,12 @@ import javax.swing.*;
 public class Main {
 	public static void main(String[] args) {
 		GamePuzzle g = new GamePuzzle();
-		JFrame frame = new JFrame("Puzzle Screen");		
+		JFrame frame = new JFrame("Puzzle Screen");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.add(g);
-		frame.setSize(GamePuzzle.WIDTH, GamePuzzle.HEIGHT);
+		frame.pack();
 		frame.setVisible(true);
 		frame.setResizable(false);
-		frame.setLocationRelativeTo(null);
 		g.start();
 	}
 }
