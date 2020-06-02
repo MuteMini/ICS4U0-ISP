29cbb291f8039f15e226595cd503eafdfafe1719
MuteMini <63823931+MuteMini@users.noreply.github.com>
2020-06-02 01:35:44 -0400

    Worked in Luggage and Luggageman class

--- bus-conductor/src/riders/Luggage.java
+++ bus-conductor/src/riders/Luggage.java
@@ -1,19 +1,139 @@
 package riders;
 
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
 
 public class Luggage extends Passenger {
 
 	protected int type;
+	protected int offX;
+	protected int offY;
 	
 	public Luggage(int orderX, int orderY, int id, int type, Color cl) {
-		super("luggage"+type+".png", id, orderX, orderY, cl);
+		super(9, type, -(id+2), orderX, orderY, cl);
+		this.ableToSelect = false;
 		this.type = type;
+		this.offX = (type != 2) ? 1 : 0;
+		this.offY = (type != 1) ? 1 : 0;
 	}
 	
 	public Luggage(int xPos, int yPos, int id, int type) {
-		super("luggage"+type+".png", id, xPos, yPos);
+		super(9, type, -(id+2), xPos, yPos);
+		this.ableToSelect = false;
 		this.type = type;
 	}
 	
+	@Override
+	public void update(Integer[][] grid) {
+		placeable = isCorrect(grid);
+		if(!ableToSelect) {
+			for(int i = 0; i < 5; i++) {
+				for(int j = 0; j < 11; j++) {
+					if(grid[i][j] == -(id+2))
+						ableToSelect = true;
+				}
+			}
+		}
+	}
+	
+	@Override
+	public boolean move(Integer[][] grid, KeyEvent e) {
+		if (xPos > 0 && (e.getKeyCode() == KeyEvent.VK_A || e.getKeyCode() == KeyEvent.VK_LEFT)) {
+			xPos--;
+			return true;
+		}
+		else if (xPos < MAX_X-offX && (e.getKeyCode() == KeyEvent.VK_D || e.getKeyCode() == KeyEvent.VK_RIGHT)) {
+			xPos++;
+			return true;
+		}
+		else if (yPos > 0 && (e.getKeyCode() == KeyEvent.VK_W || e.getKeyCode() == KeyEvent.VK_UP)) {
+			yPos--;
+			return true;
+		}
+		else if (yPos < MAX_Y-offY && (e.getKeyCode() == KeyEvent.VK_S || e.getKeyCode() == KeyEvent.VK_DOWN)) {
+			yPos++;
+			return true;
+		}
+		return false;
+	}
+	
+	@Override
+	public boolean isCorrect(Integer[][] grid) {
+		boolean nextToParent = false;
+		for(int i = 0; i < 4; i++) {
+			int tempXPos = (i == 1 || i == 3) ? xPos+1 : xPos;
+			int tempYPos = (i == 2 || i == 3) ? yPos+1 : yPos;
+			if(posExist(i)) {
+				if(selected && (grid[tempXPos][tempYPos] > 0 || grid[tempXPos][tempYPos] == BAGGAGE || grid[tempXPos][tempYPos] <= -4))
+					return false;
+				if((tempXPos > 0 && grid[tempXPos-1][tempYPos] == -(id+2))
+					|| (tempXPos < MAX_X && grid[tempXPos+1][tempYPos] == -(id+2))
+					|| (!belowWindow(tempXPos, tempYPos) && tempYPos > 0 && grid[tempXPos][tempYPos-1] == -(id+2))
+					|| (!aboveWindow(tempXPos, tempYPos) && tempYPos < MAX_Y && grid[tempXPos][tempYPos+1] == -(id+2))) {
+						nextToParent = true;
+				}
+			}
+		}
+		return nextToParent;
+	}
+	
+	@Override
+	public void fillDistance (Integer[][] grid) {
+		for(int i = 0; i < 4; i++) {
+			int tempXPos = (i == 1 || i == 3) ? xPos+1 : xPos;
+			int tempYPos = (i == 2 || i == 3) ? yPos+1 : yPos;
+			if(posExist(i)) {
+				grid[tempXPos][tempYPos] = id;
+			}
+		}
+	}
+	
+	@Override
+	protected void highlight(Graphics g, int xPosNew, int yPosNew) {
+		if(selected) {
+			if(inGrid) {
+				if(placeable)
+					g.setColor(new Color(25, 255, 25, 100));
+				else
+					g.setColor(new Color(255, 25, 25, 100));
+			}
+			else {
+				if(ableToSelect)
+					g.setColor(new Color(255, 127, 156, 120));
+				else
+					g.setColor(new Color(255, 25, 25, 120));
+			}
+			if(type == 1)
+				g.fillRoundRect(xPosNew, yPosNew, SPRITE_SIZE*2, SPRITE_SIZE, 20, 20);
+			else if(type == 2)
+				g.fillRoundRect(xPosNew, yPosNew, SPRITE_SIZE, SPRITE_SIZE*2, 20, 20);
+			else if(type == 7)
+				g.fillRoundRect(xPosNew, yPosNew, SPRITE_SIZE*2, SPRITE_SIZE*2, 20, 20);
+			else {
+				for(int i = 0; i < 4; i++) {
+					int shiftX = (i == 1 || i == 3) ? 1 : 0;
+					int shiftY = (i == 2 || i == 3) ? 1 : 0;
+					if((i==0 && type!=6) || (i==1 && type!=4)|| (i==2 && type!=5)|| (i==3 && type!=3))
+						g.fillRoundRect(xPosNew+(SPRITE_SIZE*shiftX), yPosNew+(SPRITE_SIZE*shiftY), SPRITE_SIZE, SPRITE_SIZE, 15, 15);
+				}
+			}
+		}
+	}
+	
+	@Override
+	protected void drawTag(Graphics g, int xPos, int yPos) {
+		g.setColor(cl);
+		if(type == 6)
+			g.fillOval(xPos+SPRITE_SIZE, yPos, 10, 10);
+		else
+			g.fillOval(xPos, yPos, 10, 10);
+	}
+			
+	protected boolean posExist(int i) {
+		return (i==0 && type!=6) 
+				|| (i==1 && (type!=2 && type!=4)) 
+				|| (i==2 && (type!=1 && type!=5)) 
+				|| (i==3 && (type!=1 && type!=2 && type!=3));
+	}
 }
--- bus-conductor/src/riders/Luggageman.java
+++ bus-conductor/src/riders/Luggageman.java
@@ -5,12 +5,42 @@ import java.awt.Color;
 public class Luggageman extends Passenger {
 
 	public Luggageman(int orderX, int orderY, int id, Color cl) {
-		super("luggageman.png", id, orderX, orderY, cl);
+		super(8, 0, id, orderX, orderY, cl);
 	}
 	
 	public Luggageman(int xPos, int yPos, int id) {
-		super("luggageman.png", id, xPos, yPos);
+		super(8, 0, id, xPos, yPos);
 	}
 	
+	@Override
+	public void update(Integer[][] grid) {
+		placeable = super.isCorrect(grid);
+	}
 	
+	@Override
+	public boolean isCorrect(Integer[][] grid) {
+		boolean surrounding = (grid[xPos][yPos] == 0 || (!selected && grid[xPos][yPos] == id))
+							&& (xPos == 0 || grid[xPos-1][yPos] <= 0) 
+							&& (xPos == MAX_X || grid[xPos+1][yPos] <= 0) 
+							&& (yPos == 0 || belowWindow(xPos, yPos) || grid[xPos][yPos-1] <= 0) 
+							&& (yPos == MAX_Y || aboveWindow(xPos, yPos) || grid[xPos][yPos+1] <= 0);
+		boolean nextToBaggage = (xPos > 0 && grid[xPos-1][yPos] == -(id+2)) 
+							|| (xPos < MAX_X && grid[xPos+1][yPos] == -(id+2)) 
+							|| (yPos > 0 && !belowWindow(xPos, yPos) && grid[xPos][yPos-1] == -(id+2)) 
+							|| (yPos < MAX_Y && !aboveWindow(xPos, yPos) && grid[xPos][yPos+1] == -(id+2));
+		return surrounding && nextToBaggage;
+	}
+	
+	@Override
+	public void fillDistance (Integer[][] grid) {
+		grid[xPos][yPos] = id;
+		if(xPos > 0 && grid[xPos-1][yPos] == 0)
+			grid[xPos-1][yPos] = CHILD_SPACE;
+		if(xPos < MAX_X && grid[xPos+1][yPos] == 0)
+			grid[xPos+1][yPos] = CHILD_SPACE;
+		if(!belowWindow(xPos, yPos) && yPos > 0 && grid[xPos][yPos-1] == 0)
+			grid[xPos][yPos-1] = CHILD_SPACE;
+		if(!aboveWindow(xPos, yPos) && yPos < MAX_Y && grid[xPos][yPos+1] == 0)
+			grid[xPos][yPos+1] = CHILD_SPACE;
+	}
 }
