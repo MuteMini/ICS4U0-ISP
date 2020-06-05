6dba6142e328d4232556d75612b9190f3b145a34
MuteMini <63823931+MuteMini@users.noreply.github.com>
2020-06-05 16:28:48 -0400

    renamed and added final images

--- bus-conductor/src/game/Loader.java
+++ bus-conductor/src/game/Loader.java
@@ -37,25 +37,25 @@ public final class Loader {
 	public static final BufferedImage LUGGAGE6 = getPuzzleSprite(9, 6);
 	public static final BufferedImage LUGGAGE7 = getPuzzleSprite(9, 7);
 	
+	public static final BufferedImage BUS_SPRITE = getBusSprite(0, 0);
+	public static final BufferedImage CAR_SPRITE1 = getBusSprite(1, 1);
+	public static final BufferedImage CAR_SPRITE2 = getBusSprite(1, 2);
+	public static final BufferedImage CAR_SPRITE3 = getBusSprite(1, 3);
+	public static final BufferedImage CAR_SPRITE4 = getBusSprite(1, 4);
+	public static final BufferedImage CAR_SPRITE5 = getBusSprite(1, 5);
+	public static final BufferedImage CAR_SPRITE6 = getBusSprite(1, 6);
+	public static final BufferedImage CAR_SPRITE7 = getBusSprite(1, 7);
+	public static final BufferedImage CAR_SPRITE8 = getBusSprite(1, 8);
+	public static final BufferedImage TUT_WORLD = getBusSprite(2, 1);
+	public static final BufferedImage WORLD1 = getBusSprite(2, 2);
+	
+	public static final BufferedImage SPLASH1 = getGeneralSprite(0, 1);
+	
 	public static final Font BALSAMIQ_TITLE = getFont(1).deriveFont(42f);
 	public static final Font CALIBRI_BODY1 = getFont(2);
 	public static final Font CALIBRI_BODY2 = getFont(3);
 	
 	private Loader() {
-	}
-	
-	private static Font getFont(int fontID){
-		try {
-			if(fontID == 1)
-				return Font.createFont(Font.TRUETYPE_FONT, Loader.class.getResource("/BalsamiqSans-Regular.ttf").openStream());
-			if(fontID == 2)
-				return new Font("Calibri", 1, 18);
-			if(fontID == 3)
-				return new Font("Calibri", 0, 12);
-		}
-		catch(Exception e) {
-		}
-		return null;
 	}
 	
 	private static BufferedImage getPuzzleSprite(int spriteID, int diff){
@@ -89,6 +89,43 @@ public final class Loader {
 			else if(spriteID == 9)
 				return ImageIO.read(Loader.class.getResource("/luggage"+diff+".png"));
 		} catch (IOException e) {
+		}
+		return null;
+	}
+	
+	private static BufferedImage getBusSprite(int spriteID, int diff){
+		try {	
+			if(spriteID == 0)
+				return ImageIO.read(Loader.class.getResource("/splashscreen"+diff+".png"));
+		} catch (IOException e) {
+		}
+		return null;
+	}
+	
+	private static BufferedImage getGeneralSprite(int spriteID, int diff){
+		try {	
+			if(spriteID == 0)
+				return ImageIO.read(Loader.class.getResource("/bus.png"));
+			else if(spriteID == 1) {
+				if(diff == 8)
+					return ImageIO.read(Loader.class.getResource("/carthanos.png"));
+				return ImageIO.read(Loader.class.getResource("/car"+diff+".png"));
+			}
+		} catch (IOException e) {
+		}
+		return null;
+	}
+	
+	private static Font getFont(int fontID){
+		try {
+			if(fontID == 1)
+				return Font.createFont(Font.TRUETYPE_FONT, Loader.class.getResource("/BalsamiqSans-Regular.ttf").openStream());
+			if(fontID == 2)
+				return new Font("Calibri", 1, 18);
+			if(fontID == 3)
+				return new Font("Calibri", 0, 12);
+		}
+		catch(Exception e) {
 		}
 		return null;
 	}
--- bus-conductor/src/game/SplashScreen.java
+++ bus-conductor/src/game/SplashScreen.java
@@ -1,28 +1,16 @@
 package game;
 
-
-
 import java.awt.AlphaComposite;
 import java.awt.Color;
 import java.awt.Graphics2D;
-import java.awt.image.BufferedImage;
-import java.io.IOException;
-import java.net.URL;
-import javax.imageio.ImageIO;
 
 public class SplashScreen {
-	private BufferedImage logo;
 	private boolean loadingDone;
 	private float alpha;
 
 	public SplashScreen() {
 		alpha = 0;
 		loadingDone = false;
-		try {
-			URL splashScreenLink = SplashScreen.class.getResource("/SplashScreen.png");
-			logo = ImageIO.read(splashScreenLink);
-		} catch (IOException e) {
-		}
 	}
 
 	public void update() {
@@ -35,7 +23,7 @@ public class SplashScreen {
 		g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, alpha));
 		g2d.setColor(Color.BLACK);
 		g2d.fillRect(0, 0, Game.WIDTH, Game.HEIGHT);
-		g2d.drawImage(logo, 0, 0, null);
+		g2d.drawImage(Loader.SPLASH1, 0, 0, null);
 	}
 
 	/**
