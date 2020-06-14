5d3d875c08f92eda019c6a0c2f71bc5d28f03775
MuteMini <63823931+MuteMini@users.noreply.github.com>
2020-06-14 17:46:07 -0400

    added javadocs

--- bus-conductor/src/com/truenorth/game/states/States.java
+++ bus-conductor/src/com/truenorth/game/states/States.java
@@ -3,13 +3,50 @@ package com.truenorth.game.states;
 import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
 
+/**
+ * This class holds methods to be created later when implemented.<br>
+ * 
+ * Hours Spent: ~0.5 hours <br>
+ *
+ * June 6th: Created file, Min <br>
+ * June 14th: Final comments, Min <br>
+ * 
+ * @author Min
+ */
 public interface States {
 	
+	/**
+	 * Updates the state.
+	 * 
+	 * @author Min
+	 * @since May 30th
+	 */
 	public void update();
 	
+	/**
+	 * Renders the state.
+	 * 
+	 * @param g2d the graphics to be drawn to
+	 * @author Min
+	 * @since May 30th
+	 */
 	public void render(Graphics2D g2d);
 	
+	/**
+	 * Checks for key pressed events for the state.
+	 * 
+	 * @param e the KeyEvent to process
+	 * @author Min
+	 * @since May 30th
+	 */
 	public void keyPressed(KeyEvent e);
 	
+	/**
+	 * Checks for key released events for the state.
+	 * 
+	 * @param e the KeyEvent to process
+	 * @author Min
+	 * @since May 30th
+	 */
 	public void keyReleased(KeyEvent e);
 }
--- bus-conductor/src/com/truenorth/puzzles/PuzzleState.java
+++ bus-conductor/src/com/truenorth/puzzles/PuzzleState.java
@@ -7,18 +7,48 @@ import com.truenorth.game.Loader;
 import com.truenorth.game.states.States;
 import com.truenorth.puzzles.level.*;
 
+/**
+ * This class holds all of the levels that can be played in the game. It allows communication
+ * between the levels itself and the main game loop to the levels.<br>
+ * 
+ * Hours Spent: ~9 hours <br>
+ *
+ * May 30th: Created file, Min <br>
+ * June 1st: Added getters and setters. Min <br>
+ * June 6th: Implemented States interface, Min <br>
+ * June 8th: Added isImpossible method for bug fix, Min <br>
+ * June 14th: Final comments, Min <br>
+ * 
+ * @author Min
+ */
 public class PuzzleState implements States{
 	
+	/**The amount of levels in the game and in the array*/
 	final private int LEVEL_NUM = 20;
+	/**Holds what array index the level being check out is at*/
 	private int levelPos;
+	/**Holds all of the levels that can be played*/
 	private Level[] levels;
 	
+	/**
+	 * Creates the levels array and populates it with the
+	 * subclasses of the Level class.
+	 * 
+	 * @author Min
+	 * @since May 30th
+	 */
 	public PuzzleState() {
 		this.levelPos = 7;
 		this.levels = new Level[LEVEL_NUM];
 		resetLevels();
 	}
 	
+	/**
+	 * Populates levels with the subclasses of the Level class.
+	 * 
+	 * @author Min
+	 * @since May 30th
+	 */
 	public void resetLevels() {
 		this.levels[0] = new LevelOneOne();
 		this.levels[1] = new LevelOneTwo();
@@ -42,41 +72,98 @@ public class PuzzleState implements States{
 		this.levels[19] = new LevelTwoThirteen();
 	}
 	
+	/**
+	 * Updates the current index of the level that is being played
+	 * 
+	 * @author Min
+	 * @since May 30th
+	 */
 	@Override
 	public void update() {
 		levels[levelPos].update();
 	}
 	
+	/**
+	 * Renders the current level being played.
+	 * 
+	 * @author Min
+	 * @since May 30th
+	 */
 	@Override
 	public void render(Graphics2D g2d) {
 		g2d.drawImage(Loader.PUZZLE_BACKGROUND, 0, 0, null);
 		levels[levelPos].render(g2d);
 	}
 	
+	/**
+	 * Processes the key event given to the level that is being played.
+	 * 
+	 * @param e the KeyEvent to process
+	 * @author Min
+	 * @since May 30th
+	 */
 	@Override
 	public void keyPressed(KeyEvent e) {
 		levels[levelPos].processMovement(e);
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void keyReleased(KeyEvent e) {}
 	
+	/**
+	 * A setter for the levelPos variable
+	 * 
+	 * @param levelPos the value to replace levelPos with
+	 * @author Min
+	 * @since June 1st
+	 */
 	public void setLevelPos(int levelPos) {
 		this.levelPos = levelPos;
 	}
 
+	/**
+	 * A getter for the levelPos
+	 * 
+	 * @return the level position
+	 * @author Min
+	 * @since June 1st
+	 */
 	public int getLevelPos() {
 		return levelPos;
 	}
 	
+	/**
+	 * A getter for the finished variable in the current level.
+	 * 
+	 * @return isFinished method for the current level
+	 * @author Min
+	 * @since June 1st
+	 */
 	public boolean isFinished() {
 		return levels[levelPos].isFinished();
 	}
 	
+	/**
+	 * A getter for the impossible variable in the current level.
+	 * 
+	 * @return isImpossible method for the current level
+	 * @author Min
+	 * @since June 8st
+	 */
 	public boolean isImpossible() {
 		return levels[levelPos].isImpossible();
 	}
 	
+	/**
+	 * A getter for the tutorial variable in the current level.
+	 * 
+	 * @return getTutorial method for the current level
+	 * @author Min
+	 * @since June 1st
+	 */
 	public boolean hasTutorial() {
 		return levels[levelPos].getTutorial();
 	}
